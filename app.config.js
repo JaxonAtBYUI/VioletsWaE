93725d196b248d4f5f69732209efe1493784ad9e
JaxonAtBYUI <91147570+JaxonAtBYUI@users.noreply.github.com>
2024-01-19 20:39:55 -0700

    firebase changes

--- app.config.js
+++ app.config.js
@@ -1,4 +1,6 @@
-{
+import 'dotenv/config'
+
+export default {
   "expo": {
     "name": "VioletsWaE",
     "slug": "VioletsWaE",
@@ -25,6 +27,15 @@
     },
     "web": {
       "favicon": "./assets/favicon.png"
+    },
+    "extra": {
+        firebaseApiKey: process.env.EXPO_PUBLIC_FIRBASE_API_KEY,
+        firebaseAuthDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        firebaseProjectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+        firebaseStorageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        firebaseMessagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+        firebaseAppId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+        firebaseMeasurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASURMENT_ID,
     }
   }
 }
